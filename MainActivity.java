d391d245c99ea10f2d059f435a936473da0d4140
aniketrchate <67179092+aniketrchate@users.noreply.github.com>
2020-09-10 10:10:49 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,139 @@
+package com.anir_projects.mgfashion;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.app.ActionBar;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.text.Html;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity {
+    WebView webView;
+    ProgressBar progressBarWeb;
+    private String webUrl = "your app link here !";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Window window = getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+        webView = findViewById(R.id.webview);
+        // if(savedInstanceState == null){
+        //     webView.restoreState(savedInstanceState);
+        //}
+        getSupportActionBar().setTitle(Html.fromHtml("<font color=\"red\">"
+                + getString(R.string.app_name) + "</font>"));
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        progressBarWeb = findViewById(R.id.progressbar);
+
+        webView.loadUrl(webUrl);
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                progressBarWeb.setVisibility(View.VISIBLE);
+                progressBarWeb.setProgress(newProgress);
+                if (newProgress == 100) {
+                    progressBarWeb.setVisibility(View.GONE);
+                }
+
+                super.onProgressChanged(view, newProgress);
+            }
+        });
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean  shouldOverrideUrlLoading(WebView view, String url) {
+                if (url != null &&  (url.startsWith("whatsapp://"))) {
+                    view.getContext().startActivity(
+                            new Intent(Intent.ACTION_VIEW, Uri.parse(url.replace("+",""))));
+
+                    return true;
+
+                }
+                else if (url != null &&  (url.startsWith("intent://"))){
+                    view.getContext().startActivity(
+                            new Intent(Intent.ACTION_VIEW));
+
+                    return true;
+                }
+                    else {
+                    return false;
+                }
+            }
+        }); 
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("Want to Exit?")
+                    .setNegativeButton("No", null)
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            finishAffinity();
+                        }
+                    }).show();
+
+
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbarmenu, menu);
+        return super.onCreateOptionsMenu(menu);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+
+            case R.id.nav_reload:
+                webView.reload();
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
